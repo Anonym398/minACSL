2ab9103f2853b23b614e4010c654d1e660596419
Anonym398 <lgtest398@outlook.fr>
2023-12-14 16:48:38 +0100

    remove compiled files

--- core_c/proof_paper.v
+++ core_c/proof_paper.v
@@ -13,12 +13,36 @@ Qed.
   
 Lemma eq_paper2 : forall en, ~(comp_valt EqOp en (VInteger 8) (VInteger 0))/\
                                          (comp_valt EqOp en (VInteger (divZ 4)) (VInteger (divZ 4))).
-  Proof.
-    intros.
-    split.
-    intro excluded_middle.
-    inversion excluded_middle.
-    inversion H4.
-    apply comp_valInt.
-    apply eq_int.
-  Qed.
+Proof.
+  intros.
+  split.
+  intro excluded_middle.
+  inversion excluded_middle.
+  inversion H4.
+  apply comp_valInt.
+  apply eq_int.
+Qed.
+
+(* Axiom tructest : forall a b : Z, (if decide (a=b) then 1 else 0) = 1 -> a=b. *)
+
+(*   Print simplify_equality. *)
+(* Lemma eq_paper3 : forall en t1, *)
+(*     ~(eq_val en (VStruct t1 [intV{sintT} 2; intV{sintT} 1]) *)
+(*         (VStruct t1 [intV{sintT} 2; intV{sintT} 5])) . *)
+(* Proof. *)
+(*   intros. *)
+(*   intro excluded_middle. *)
+(*   inversion excluded_middle. *)
+(*   inversion H2. *)
+(*   inversion H12. *)
+(*   inversion H17. *)
+(*   inversion H23. *)
+(*   simplify_equality'. *)
+(*   apply (tructest (int_cast (int_promote sintT ∪ int_promote sintT) 1) *)
+(*            (int_cast (int_promote sintT ∪ int_promote sintT) 5)) in H25. *)
+(*   apply inj int_cast in H *)
+(*   discriminate H25. *)
+  
+(*   apply eq_ in excluded_middle. *)
+
+
